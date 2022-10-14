6dbc08c0914fafa6eebb0eb2c795ab4b5cb95e8e
Elida001 <elida.leite@sophos.com>
2022-10-14 09:50:13 -0400

    Forensic Snapshot

--- Artefacts/Sophos/Find Sophos Forensic Snapshots.sql
+++ Artefacts/Sophos/Find Sophos Forensic Snapshots.sql
@@ -7,17 +7,17 @@
 | https://support.sophos.com/support/s/article/KB-000038358?language=en_US       |
 |                                                                                |
 | Version: 1.0                                                                   |
-| Author: The Rapid Response Team                                                |
+| Author: The Rapid Response Team | Elida Leite                                  |
 | github.com/SophosRapidResponse                                                 |
 \********************************************************************************/
 
 SELECT
-	CASE WHEN directory LIKE '%Forensic Snapshots' THEN 'Customer-generated'
+	CASE WHEN directory LIKE '%Forensic Snapshots' THEN 'customer-generated'
 	WHEN directory LIKE '%Saved Data' THEN 'threat detection'
 	END AS 'From', 
-	directory,
 	filename,
-	size, 
+	directory,
+	round((size * 10e-7) ,2)|| ' ' || 'MB' AS 'size', 
 	datetime (btime,'unixepoch') AS creation_time,
 	datetime (mtime,'unixepoch') AS modified_time,
 	'File' AS source, 
@@ -25,3 +25,4 @@ SELECT
 FROM file
 WHERE directory IN ('C:\ProgramData\Sophos\Endpoint Defense\Data\Forensic Snapshots', 'C:\ProgramData\Sophos\Endpoint Defense\Data\Saved Data')
 	AND filename LIKE '%snapshot%.tgz'
+ORDER BY btime DESC
