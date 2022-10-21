a7a0447583a54b4a94c2052a836ff60c3e5eab4a
Elida001 <elida.leite@sophos.com>
2022-10-21 15:52:36 -0400

    update

--- Artefacts/PowerShell/Windows AMSI Events.sql
+++ Artefacts/PowerShell/Windows AMSI Events.sql
@@ -1,49 +1,46 @@
 /*************************** Sophos.com/RapidResponse ***************************\
 | DESCRIPTION                                                                    |
-| The query brings all the Windows AMSI events stored on Sophos AMSI journal     |
+| List all Windows AMSI events from Sophos AMSI journal.                         |
 | The journal supports the following scripts : PowerShell, VBA, VBScript, JScript|
 | and WMI                                                                        |
 |                                                                                |
-| VARIABLE:                                                                      |
-| - start_time (date)                                                            |
-| - end_time (date)                                                              |
+| Users can search for a specific keyword in the script body by assigning a value|
+| to the variable "ioc" or use % as a wildcard to get everything                 |
 |                                                                                |
-| Version: 1.0                                                                   |
-| Author: Sophos Team | Elida Leite                                              |
+| VARIABLE:                                                                      |
+| - start_time (type: DATE)                                                      |
+| - end_time (type: DATE)                                                        |
+| - ioc (type: STRING)                                                           |
+|                                                                                |
+| Version: 1.1                                                                   |
+| Author: The Rapid Response Team                                                |
 | github.com/SophosRapidResponse                                                 |
 \********************************************************************************/
 
 SELECT
-    STRFTIME('%Y-%m-%dT%H:%M:%SZ', DATETIME(saj.time, 'unixepoch')) AS datetime,
+    STRFTIME('%Y-%m-%dT%H:%M:%SZ', DATETIME(saj.time, 'unixepoch')) AS date_time,
+    saj.script_type,
     spj.process_name,
     spj.cmd_line,
-    saj.content As script,
-    saj.content_name As script_name,
     saj.sophos_pid,
-    saj.script_type,
-    spa.object,
-    spa.action,
-    CAST ( (Select spj2.cmd_line from sophos_process_journal spj2 where spj2.sophos_pid = spj.parent_sophos_pid) AS text) parent_process,
+    saj.content AS script,
+    saj.content_name AS script_name,    
     CASE
         WHEN LENGTH(saj.content) * 2 < saj.content_length THEN 'True'
         ELSE 'False'
-    END AS script_is_truncated,
-    saj.thumbprint As script_hash,
+    END AS script_truncated,
+    saj.thumbprint AS script_hash,
     saj.session_id,
     saj.session_part,
     users.username,
-    saj.owner As sid,
+    saj.owner AS sid,
     saj.content_length,
-    'AMSI/Process/Activity Journals' AS Data_Source,
+    spj.parent_sophos_pid,
+    CAST ( (SELECT spj2.cmd_line FROM sophos_process_journal spj2 WHERE spj2.sophos_pid = spj.parent_sophos_pid) AS TEXT) parent_cmdline,
     'T1027 - Windows AMSI Events' AS Query
-FROM sophos_amsi_journal AS saj
-LEFT JOIN
-    sophos_process_journal AS spj ON spj.sophos_pid = saj.sophos_pid
-LEFT JOIN
-    users ON users.uuid = saj.owner
-LEFT JOIN 
-    sophos_process_activity spa ON spa.sophos_pid = spj.sophos_pid
-WHERE 
-    saj.time >= '$$start_time$$' AND saj.time <= '$$end_time$$'
-    AND NOT (saj.thumbprint = 'e60ecfdf0ff394e19092e896ec7a5342c35ec94819a6c5a176e8893ac1b0bf43' AND parent_process LIKE '%CompatTelRunner.exe -m:appraiser.dll -f:DoScheduledTelemetryRun%')
-    GROUP BY saj.sophos_pid
+FROM sophos_amsi_journal saj
+LEFT JOIN sophos_process_journal spj USING(sophos_pid)
+LEFT JOIN users ON users.uuid = saj.owner
+WHERE saj.content LIKE '$$ioc$$' 
+    AND saj.time >= '$$start_time$$' 
+    AND saj.time <= '$$end_time$$'
