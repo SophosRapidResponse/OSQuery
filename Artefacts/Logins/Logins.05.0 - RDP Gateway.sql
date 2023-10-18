a124013346a9cb76bc92629074dc7ce39274ab48
Elida001 <elida.leite@sophos.com>
2023-10-18 14:19:18 -0400

    improved description: Logins.05.0

--- Artefacts/Logins/Logins.05.0 - RDP Gateway.sql
+++ Artefacts/Logins/Logins.05.0 - RDP Gateway.sql
@@ -1,9 +1,10 @@
 /*************************** Sophos.com/RapidResponse ***************************\
 | DESCRIPTION                                                                    |
-| Lists all successful authentication logs (EID 302) from the Remote Desktop (RD)|
+| List all successful authentication logs (EID 302) from the Remote Desktop (RD) |
 | Gateway event logs: Microsoft-Windows-TerminalServices-Gateway/Operational.    |
 |                                                                                |
-| Brings user information, device information, and connection details            |
+| Should be run on servers that have the Remote Desktop Gateway service installed|
+| The query returns user information, device information, and connection details |
 |                                                                                |
 | Version: 1.0                                                                   |
 | Author: The Rapid Response Team                                                |
@@ -20,7 +21,7 @@ JSON_EXTRACT(data,'$.UserData.Resource') AS device_name,
 JSON_EXTRACT(data,'$.UserData.AuthType') AS auth_type,
 JSON_EXTRACT(data,'$.UserData.ErrorCode') AS error_code,
 JSON_EXTRACT(data,'$.UserData.EventInfo') AS event_info,
-source,
+'TS Gateway EVTX' AS data_source,
 'Logins.05.0' AS query
 FROM sophos_windows_events
 WHERE source = 'Microsoft-Windows-TerminalServices-Gateway/Operational'
