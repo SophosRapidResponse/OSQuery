9d10252204a6ef6e6590571289544405fc0cda03
Elida001 <elida.leite@sophos.com>
2023-10-18 11:53:58 -0400

    added description: device.07.0

--- Artefacts/Device/Device.07.0 - Active Directory Powershell Module.sql
+++ Artefacts/Device/Device.07.0 - Active Directory Powershell Module.sql
@@ -1,3 +1,15 @@
+/*************************** Sophos.com/RapidResponse ***************************\
+| DESCRIPTION                                                                    |
+| List all devices that have installed/enabled the Active Directory PowerShell   |
+| module. The module is automatically installed on domain controllers, however,  |
+| it can be installed on other devices to allow remote management of the Active  |
+| Directory environment                                                          |
+|                                                                                |
+| Version: 1.0                                                                   |
+| Author: The Rapid Response Team                                                |
+| github.com/SophosRapidResponse                                                 |
+\********************************************************************************/
+
 SELECT
 name AS feature_name,
 caption,
